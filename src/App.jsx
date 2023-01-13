f33b6d08e42ed27b22371e3d6e95d2b51f7649bc
Atala <atala.rchidi@gmail.com>
2023-01-12 21:18:37 -0500

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <div className="App">
-      <h1 className='display-1'>Hello Aata</h1>
-    </div>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
